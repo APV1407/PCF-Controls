8b748257634e734de02a1e0c69b89bdbcbe6c320
APV1407 <v-apratap@microsoft.com>
2021-04-14 19:22:00 +0530

    Regex Validation PCF Control

---
+++ RegexValidation/NTBRegexValidation/generated/ManifestTypes.d.ts
@@ -0,0 +1,13 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    ValueToProcess: ComponentFramework.PropertyTypes.StringProperty;
+    RegexExpression: ComponentFramework.PropertyTypes.StringProperty;
+    UserNotificationText: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    ValueToProcess?: string;
+}
